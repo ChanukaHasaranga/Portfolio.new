09155ce8b4b2c263c6f032e1699b96e1889582b3
nimthara <96354974+ChanukaHasaranga@users.noreply.github.com>
2025-09-24 02:07:21 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Portfolio_new/", // <--- GitHub repo name
+  base: "/Portfolio_new/", // important for GitHub Pages
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
