accade5289ca4a1a4a626c598bfb13be1fe558b9
nimthara <96354974+ChanukaHasaranga@users.noreply.github.com>
2025-09-24 02:04:13 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// Replace 'your-repo-name' with your GitHub repo name
-const repoName = "/Portfolio_new/";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: repoName, // <--- Add this line for GitHub Pages
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  base: "/Portfolio_new/", // <--- GitHub repo name
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
