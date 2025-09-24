28aaa7488c42029ce68429f786b241e6abc558f9
nimthara <96354974+ChanukaHasaranga@users.noreply.github.com>
2025-09-24 15:30:33 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/Portfolio_new/",  //  must match repo name
+  base: "/Portfolio.new/",  //  must match repo name
   plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") }
